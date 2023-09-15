40b3ee2e4478533c41a4825bde5325fea3f98a97
Nicole <rosanik3103@gmail.com>
2023-09-15 20:48:28 -0300

    Adciona exercicio de decomposição decimal

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿ int NumeroDigitado, 
+    Centena,
+    Dezena,
+    Unidade;
+
+ Console.WriteLine
+    ("--- Decomposição Decimal ---");
+
+Console.WriteLine
+    ("Digite um número inteiro...: ");
+
+NumeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine
+    ($"\nO número {NumeroDigitado} possui: ");
+
+Centena = NumeroDigitado / 100;
+
+Dezena = (NumeroDigitado % 100) / 10;
+
+Unidade = (NumeroDigitado % 100) % 10;
+
+Console.WriteLine
+    ($"{Unidade,10} Unidade(s)");
+
+Console.WriteLine
+    ($"{Dezena,10} Dezenas(s)");
+
+Console.WriteLine
+    ($"{Centena,10} Centena(s)");
